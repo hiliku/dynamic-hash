39010a117bb479683191ad81fe13ca63573e4890
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-17 10:40:01 +0300

    getSalt

--- index.js
+++ index.js
@@ -2,30 +2,46 @@
 const crypto = require("crypto");
 const fs = require("fs");
 
+const saltFile = "./salt/prase.json";
+
+// generate random hash string by timestamp
+genHash = async (type = "sha256") => { // type = md5 | sha1 | sha256
+  let time = Date.now();
+  let hash = crypto.createHash(type);
+  hash.update(String(time));
+  let str = await hash.digest("hex");
+  return str;
+};
+
 module.exports = {
+  // gen random salt string
+  genSalt: async () => {
+    const salt = await genHash();
+    const time = Date.now();
+    const data = { salt, time };
 
-    // generate random hash string by timestamp
-    genHash: async (type = 'sha256') => { // type = md5 | sha1 | sha256
-        let time = new Date().getUTCMilliseconds();
-        let hash = crypto.createHash(type);
-        hash.update(String(time));
-        let str = await hash.digest("hex");
-        return str;
-    },
-    // gen random salt string
-    genSalt: async () => {
-        const salt = this.genHash();
-        // delete local prase file
-        try {
-            fs.unlinkSync('./salt/prase.json');
-            console.log('successfully deleted prase.json');
-        } catch (err) {
-            console.log('no such file')
-        }
-
-    },
-    getSalt: () => {
-        
+    // delete local prase file
+    try {
+      fs.unlinkSync(saltFile);
+    } catch (err) {
+      console.log("no such file");
     }
+    // create local prase file with new salt
+    try {
+      fs.writeFileSync(saltFile, JSON.stringify(data));
+    } catch (err) {
+      console.log(err);
+    }
+  },
 
-}
+  // get current salt prase
+  getSalt: async () => {
+    fs.open(saltFile, "r", (err, fd) => {
+      if (err) throw err;
+      fs.close(fd, err => {
+        if (err) throw err;
+        console.log(fd)
+      });
+    });
+  }
+};
