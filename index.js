ca6845cdc8295ff876eb0d2b0019582ceffd3ee3
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-17 09:04:30 +0300

    adding genHash and genSalt files

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // "use strict";
 const crypto = require("crypto");
-const pool = require("./connect");
+const fs = require("fs");
 
-module.exports = {}
+module.exports = {
+
+    // generate random hash string by timestamp
+    genHash: async (type = 'sha256') => { // type = md5 | sha1 | sha256
+        let time = new Date().getUTCMilliseconds();
+        let hash = crypto.createHash(type);
+        hash.update(String(time));
+        let str = await hash.digest("hex");
+        return str;
+    },
+    // gen random salt string
+    genSalt: async () => {
+        const salt = this.genHash();
+        // delete local prase file
+        try {
+            fs.unlinkSync('./salt/prase.json');
+            console.log('successfully deleted prase.json');
+        } catch (err) {
+            console.log('no such file')
+        }
+
+    },
+    getSalt: () => {
+        
+    }
+
+}
