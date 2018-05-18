5888ca07667b59ac6a4e3453618ba09fd4b4ef82
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-18 17:26:03 +0300

    chai testing

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,64 +1,38 @@
 const dynamicHash = require("../index");
 const assert = require("chai").assert;
 
+// Value to encrypt:
+const value = 'test value to hash';
+// Generate a salt object at ./salt/prase.json
+const genSalt = dynamicHash.genSalt(); // ---> Returns the salt object
+// Get the current salt
+const salt = dynamicHash.getSalt(); // ---> Returns salt
+// Encrypting the given value
+const encrypted = dynamicHash.hashVal(value, salt); // ---> Returns hash value
+// Checking given value versus the encrypted value
+const check = dynamicHash.checkVal(value, encrypted, salt); // ---> Returns Boolean
+// Decrypting the given hash value
+const decrypted = dynamicHash.getVal(encrypted, salt); // ---> Returns original value
+
 describe("Dynamic-Hash Testing", () => {
-  const value = 'test value to hash';
-  it("Succesfully create a salt object at ./salt/prase.json", async () => {
-    // generate a salt object at ./salt/prase.json
-    const genSalt = await dynamicHash.genSalt();
-    // ---> Returns the salt object
+  it("Succesfully create a salt object at ./salt/prase.json", () => {
     assert.typeOf(genSalt, "object");
   });
-  it("Get the current salt value from ./salt/prase.json", async () => {
-    const salt = await dynamicHash.getSalt();
+  it("Get the current salt value from ./salt/prase.json", () => {
     assert.typeOf(salt, "string");
   });
 
-  it("Testing if salt is equal to current salt at ./salt/prase.json", async () => {
-    const genSalt = await dynamicHash.genSalt();
-    const salt = await dynamicHash.getSalt();
+  it("Testing if salt is equal to current salt at ./salt/prase.json", () => {
     assert.equal(salt, genSalt.salt);
   });
 
-  it("Testing if a given value is equal to a given hash", async () => {
-    const salt = await dynamicHash.getSalt();
-    // encrypting the given value
-    const encrypted = await dynamicHash.hashVal(value, salt);
-    // checking given value versus the encrypted value
-    const check = await dynamicHash.checkVal(value, encrypted, salt);
-    // ---> Returns Boolean
+  it("Testing if a given value is equal to a given hash", () => {
     assert.isTrue(check);
   });
+
+  it("Testing if the decrypted value is equal to the orignal encrypted value", () => {
+    assert.equal(decrypted,value)
+  });
+  done()
 });
 
-// test = (async () => {
-//   const value = "test";
-
-//   console.log(`-- Encrypt value with current salt -->`);
-//   const encrypted = await dynamicHash.hashVal(value, salt);
-//   console.log(`-- Encrypted value: ${encrypted}`);
-
-//   console.log(`-- Check if given value is equal to hash string -->`);
-//   const check = await dynamicHash.checkVal(value, encrypted, salt);
-
-//   // testing if a given value is equal to a given hash
-//   try {
-//     assert.ok(check)
-//   } catch (e) {
-//     console.log(`!!!!! ${e}`)
-//     return;
-//   }
-//   console.log(`-- Returns Boolean value true | false : ${check}`);
-
-//   console.log(`-- Dencrypt hash value with current salt -->`);
-//   const decrypted = await dynamicHash.getVal(encrypted, salt);
-
-//   // testing if the decrypted value is equal to the orignal encrypted value
-//   try {
-//     assert.deepEqual(decrypted,value)
-//   } catch (e) {
-//     console.log(`!!!!! ${e}`);
-//     return;
-//   }
-//   console.log(`-- Check if the original value ${value} is returned: ${decrypted}`);
-// })();
