e999a3467550539bdef6ddc300ef53bc471d49f9
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-18 10:48:11 +0300

    testing default methods

--- index.test.js
+++ index.test.js
@@ -1,7 +1,20 @@
-const hash = require('./index');
+const hash = require("./index");
+const assert = require("assert");
 
-const assert = require('assert');
-
-
-
-console.log(hash.genSalt())
+test = (async () => {
+  const val = "test";
+  const genSalt = await hash.genSalt();
+  console.log(`-- Gen new salt --> ${JSON.stringify(genSalt)}`);
+  const salt = await hash.getSalt();
+  console.log(`-- Get current salt --> ${salt}`);
+  const str = await hash.hashVal(val, salt);
+  console.log(
+    `-- Returns an hash string of the given value with the salt --> ${str}`
+  );
+  const check = await hash.checkVal(val, str, salt);
+  console.log(`-- Check if given value is equal to hash string --> ${check}`);
+  const cipher = await hash.getVal(str, salt);
+  console.log(
+    `-- Cipher the hash with the current salt --> '${cipher}' should be equal as '${val}'`
+  );
+})();
