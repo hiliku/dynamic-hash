7ac5f85f08e79364018a218411b390ef1fece951
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-18 11:51:48 +0300

    new testing methods

--- index.test.js
+++ index.test.js
@@ -3,14 +3,49 @@ const assert = require("assert");
 
 test = (async () => {
   const value = "test";
+
+  // generating new salt at ./salt/ folder
+  console.log(`-- Gen new salt -->`);
   const genSalt = await dynamicHash.genSalt();
-  console.log(`-- Gen new salt --> ${JSON.stringify(genSalt)}`);
+  console.log(`salt value:  ${JSON.stringify(genSalt)}`)
+
+  // get current salt value
+  console.log(`-- Get current salt -->`);
   const salt = await dynamicHash.getSalt();
-  console.log(`-- Get current salt --> ${salt}`);
+  console.log(`-- current salt: ${salt}`);
+
+  // testing if salt is equal to current salt at ./salt/prase.json
+  try {
+    assert.deepEqual(genSalt.salt,salt)
+  } catch (e) {
+    console.log(`!!!!! ${e}`)
+    return;
+  }
+  console.log(`-- Encrypt value with current salt -->`);
   const encrypted = await dynamicHash.hashVal(value, salt);
-  console.log(`-- Returns an hash string of the given value with the salt --> ${encrypted}`);
+  console.log(`-- Encrypted value: ${encrypted}`);
+  
+  console.log(`-- Check if given value is equal to hash string -->`);
   const check = await dynamicHash.checkVal(value, encrypted, salt);
-  console.log(`-- Check if given value is equal to hash string --> ${check}`);
+
+  // testing if a given value is equal to a given hash 
+  try {
+    assert.ok(check)
+  } catch (e) {
+    console.log(`!!!!! ${e}`)
+    return;
+  }
+  console.log(`-- Returns Boolean value true | false : ${check}`);
+
+  console.log(`-- Dencrypt hash value with current salt -->`);
   const decrypted = await dynamicHash.getVal(encrypted, salt);
-  console.log(`-- Cipher the hash with the current salt --> '${decrypted}' should be equal as '${value}'`);
+
+  // testing if the decrypted value is equal to the orignal encrypted value
+  try {
+    assert.deepEqual(decrypted,value)
+  } catch (e) {
+    console.log(`!!!!! ${e}`);
+    return;
+  }
+  console.log(`-- Check if the original value ${value} is returned: ${decrypted}`);
 })();
