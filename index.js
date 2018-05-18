92e7b8fe7f209ab3a1dd739766df9eceb75d22b7
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-18 10:47:59 +0300

    decipher methods finish

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const crypto = require("crypto");
 const fs = require("fs");
 
 const saltFile = "./salt/prase.json";
+const method = "aes-256-cbc";
 
 // generate random hash string by timestamp
-genHash = async (type = "sha256") => { // type = md5 | sha1 | sha256
-  let time = Date.now();
-  let hash = crypto.createHash(type);
+genHash = async () => {
+  const time = Date.now();
+  const hash = crypto.createHash('sha256');
   hash.update(String(time));
-  let str = await hash.digest("hex");
-  return str;
+  const data = await hash.digest("hex");
+  return data;
 };
 
 module.exports = {
@@ -37,12 +38,34 @@ module.exports = {
 
   // get current salt prase
   getSalt: async () => {
-    fs.open(saltFile, "r", (err, fd) => {
-      if (err) throw err;
-      fs.close(fd, err => {
-        if (err) throw err;
-        console.log(fd)
-      });
-    });
+    try {
+      return JSON.parse(fs.readFileSync(saltFile, "utf8")).salt;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  // hash value with given salt
+  hashVal: async (value, salt) => {
+    let hash = crypto.createCipher(method, salt);
+    let data = hash.update(value, 'utf-8', 'hex');
+    data += hash.final('hex');
+    return data;
+  },
+
+  // reverse hash ot original value with given salt
+  getVal: async (hash, salt) => { 
+    const cipher = crypto.createDecipher(method, salt);
+    let data = cipher.update(hash, 'hex', 'utf-8');
+    data += cipher.final('utf-8');
+    return data;
+  },
+
+  // compare two values with given salt
+  checkVal: async (val, hashVal, salt) => {
+    const hash = crypto.createCipher(method, salt);
+    let data = hash.update(val, 'utf-8', 'hex');
+    data += hash.final('hex');
+    return data == hashVal;
   }
 };
