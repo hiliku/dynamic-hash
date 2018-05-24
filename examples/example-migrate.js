513043c6ce44cbd320bdb9db0f5f69c711e10fdf
Yehiel Amar <yehiel.amar@gmail.com>
2018-05-24 14:13:11 +0300

    backup

--- examples/example-migrate.js
+++ examples/example-migrate.js
@@ -7,44 +7,60 @@ const fs = require('fs');
     // -----------------------
 
     // creating salt to secure the data
-    let currentSalt = dynamicHash.genSalt().salt;
+    try {
+        let currentSalt = dynamicHash.getSalt(() => {
+            currentSalt = dynamicHash.genSalt().salt;
+        });
+        // getting the current secured data
+        const currentUsers = JSON.parse(fs.readFileSync('examples/db.json', 'utf8')).users;
+        try {
+            // backing up current data into a backup_*.json file
+            const backup = dynamicHash.backup(JSON.stringify('currentUsers'),'json');
+            if (backup) {
+                // replacing sensitive data with encrypted values with current salt
+                currentUsers.map(value => value.fullName = dynamicHash.encrypt(value.fullName, currentSalt))
+                // updated db:
+                console.log('')
+                console.log('current users:')
+                console.log(currentUsers)
+                console.log('---------------------------------------------')
+                console.log('')
+            
+                // -------------------------
+                // SIMULATING DATA MIGRATION
+                // -------------------------
+            
+                // get the current salt
+                currentSalt = dynamicHash.getSalt();
+                // generate new salt Object and get the salt attribute
+                const newSalt = dynamicHash.genSalt().salt;
+                // once you generate new salt - you must update your database
+            
+                console.log('-- starting migration...')
+                console.log('')
+                // fetching all users from the example DB
+                const users = currentUsers;
+            
+                // decrypting sensetive data
+                users.map(user => user.fullName = dynamicHash.decrypt(user.fullName,currentSalt));
+                console.log('-- decrypted DB before migration:')
+                console.log(users)
+                console.log('---------------------------------------------')
+                console.log('')
+                console.log('-- encrypting users fullName attribute in db with new salt key...')
+                // replacing sensitive data with encrypted values with current salt
+                users.map(user => user.fullName = dynamicHash.encrypt(user.fullName,newSalt))
+                console.log('')
+                console.log('-- DB after migration:')
+                // updated db:
+                console.log(users)
+            }
+        } catch (error) {
+            console.log(error)
+            return
+        }
+    } catch (error) {
+    }
 
-    // getting the current secured data
-    const currentUsers = JSON.parse(fs.readFileSync('examples/db.json', 'utf8')).users;
-    // replacing sensitive data with encrypted values with current salt
-    currentUsers.map(value => value.fullName = dynamicHash.encrypt(value.fullName, currentSalt))
-    // updated db:
-    console.log('')
-    console.log('current users:')
-    console.log(currentUsers)
-    console.log('---------------------------------------------')
-    console.log('')
-
-    // -------------------------
-    // SIMULATING DATA MIGRATION
-    // -------------------------
-
-    // get the current salt
-    currentSalt = dynamicHash.getSalt();
-    // generate new salt Object and get the salt attribute
-    const newSalt = dynamicHash.genSalt().salt;
-    // once you generate new salt - you must update your database
-
-    console.log('-- starting migration...')
-    console.log('')
-    // fetching all users from the example DB
-    const users = currentUsers;
-    // decrypting sensetive data
-    users.map(user => user.fullName = dynamicHash.decrypt(user.fullName,currentSalt));
-    console.log('-- decrypted DB before migration:')
-    console.log(users)
-    console.log('---------------------------------------------')
-    console.log('')
-    console.log('-- encrypting users fullName attribute in db with new salt key...')
-    // replacing sensitive data with encrypted values with current salt
-    users.map(user => user.fullName = dynamicHash.encrypt(user.fullName,newSalt))
-    console.log('')
-    console.log('-- DB after migration:')
-    // updated db:
-    console.log(users)
+    
 })()
--- examples/example-secure.js
+++ examples/example-secure.js
@@ -10,6 +10,8 @@ const fs = require('fs');
     console.log('')
     // fetching all users from the example DB
     const users = JSON.parse(fs.readFileSync('examples/db.json', 'utf8')).users;
+    // backing up current data into a backup_*.json file
+    const backup = dynamicHash.backup(JSON.stringify(users),'json');
     console.log('-- securing users fullName attribute in db...');
     console.log('---------------------------------------------')
     console.log('')
